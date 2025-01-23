b57d41d67e732548a2b0f89ea7b85133005aa812
Claaarree <clare.fh@hotmail.com>
2025-01-23 17:08:20 +0800

    initial commit

---
+++ src/test/java/vttp5a_paf/day26ws/Day26wsApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp5a_paf.day26ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day26wsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
